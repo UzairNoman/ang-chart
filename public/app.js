6e63aef72b4aa1899614e5817b70ba2b0f7a2e75
UzairNoman <uzairation@gmail.com>
2017-05-17 12:11:43 +0500

    node api tested

--- public/app.js
+++ public/app.js
@@ -1,28 +1,25 @@
 (function(){
     'use strict';
     angular.module('ang-chart',[])
-            .controller('ChartController',ChartController);
-    function ChartController($scope){
-        this.data = {
-            labels: ["January", "February", "March", "April", "May", "June"],
-            datasets: [
-                {
-                    fillColor: "rgba(220,220,220,0.2)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
-                    data: [40, 10, 60, 70, 20, 20]
-                },
-                {
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    data: [30, 70, 40, 90, 60, 70]
-                }
-            ]
-        };
-        // $scope.show = show;
-        // function show(chart){
-        //     console.log(chart);
-        // }
-    }        
+            .controller('ChartController',ChartController).config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);  ;
+    function ChartController($scope,$http){  
+        $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+        $scope.show = function(chart,$event) {
+            $event.preventDefault();
+            console.log(chart);
+            $http({
+                method: 'POST',
+                url: '/csv',
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function (data, status, headers, config) {
+                console.log(data);
+            }).error(function (data, status, headers, config) {
+                // handle error things
+            });
+        }; 
+        
+    }
+          
 })();
--- server.js
+++ server.js
@@ -1,5 +1,57 @@
 var express = require('express');
 var app = express();
+var multer  = require('multer')
+var parse = require('csv-parse');
+var fs = require('fs');
+var cors = require('cors');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var upload = multer({dest : '/public/uploads'});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+// router.post('/csv1',function(req,res){
+//     console.log("hey");
+//     res.json({ 
+//         bitch : 'heuyasd'
+
+//     });
+// });
+
+router.post('/csv',upload.single('myfile'),function(req,res){
+    var file  = req.file;
+
+    
+    fs.createReadStream(file.path).pipe(parse()).on('data',function(data){
+        console.log(data + "this is data");
+    });
+
+    rs = fs.createReadStream(file.path);
+    parser = parse({columns: true}, function(err, data){
+        console.log(data);
+        res.json(data);
+    })
+    rs.pipe(parser);
+});
+
+router.get('/hello',function(req,res){
+    res.json({
+        uzair : "fine"
+    });
+});
 
 app.use(express.static(__dirname + '/public'));
+
+router.post('/new',function(req,res){
+    rs = fs.createReadStream(__dirname+'/public/uploads/data-1.csv');
+    parser = parse({columns: true}, function(err, data){
+    console.log(data);
+    })
+    rs.pipe(parser);
+
+});
+app.use('/', router);
 app.listen(3000);
